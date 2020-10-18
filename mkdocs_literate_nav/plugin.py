f9da178e12257c626997d1d3202a6d373289071c
Oleh Prypin <oleh@pryp.in>
2020-10-18 02:11:49 +0200

    Simplify 'nav_file' handling, remove 'exclude' config

--- mkdocs_literate_nav/plugin.py
+++ mkdocs_literate_nav/plugin.py
@@ -8,34 +8,16 @@ from mkdocs_literate_nav import parser, util
 
 
 class LiterateNavPlugin(mkdocs.plugins.BasePlugin):
-    config_scheme = (
-        ("nav_file", mkdocs.config.config_options.File(exists=True)),
-        ("exclude", mkdocs.config.config_options.Type(bool, default=False)),
-    )
-
-    def load_config(self, *args, **kwargs):
-        errors, warnings = super().load_config(*args, **kwargs)
-        if not self.config.get("nav_file"):
-            error = mkdocs.config.base.ValidationError(
-                "literate-nav plugin requires the nav_file parameter"
-            )
-            errors.append(("nav_file", error))
-        return errors, warnings
-
-    def on_files(self, files, config):
-        result = []
-        for f in files:
-            if self.config["exclude"] and f.abs_src_path == self.config["nav_file"]:
-                continue
-            result.append(f)
-        return mkdocs.structure.files.Files(result)
+    config_scheme = (("nav_file", mkdocs.config.config_options.Type(str, required=True)),)
 
     def on_nav(self, nav, config, files):
         for page in nav.pages:
             page.previous_page = None
             page.next_page = None
 
-        items = parser.markdown_to_nav(self.config["nav_file"])
+        items = parser.markdown_to_nav(
+            files.get_file_from_path(self.config["nav_file"]).abs_src_path
+        )
         pages = []
 
         @util.collect
